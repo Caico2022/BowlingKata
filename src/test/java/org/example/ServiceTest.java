f0e37811686e4f546dcbbf430ae5d3e8a21cad41
cpaun <calvin.paunovic@web.de>
2024-11-18 14:24:31 +0100

    Expand the Junit test with more Tests and BeforeEach for a better test setup

--- src/test/java/org/example/ServiceTest.java
+++ src/test/java/org/example/ServiceTest.java
@@ -1,15 +1,34 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
 
+    private Service service;
+
+    @BeforeEach
+    void setUp() {
+        this.service = new Service();
+    }
+
     @Test
     void getNamePositiveNumber() {
-        Service service = new Service();
         String result = service.getName(5);
+        assertEquals("b", result);
+    }
+
+    @Test
+    void getNameNegativeNumber() {
+        String result = service.getName(-1);
+        assertEquals("a", result);
+    }
+
+    @Test
+    void getNameZero() {
+        String result = service.getName(0);
         assertEquals("b", result);
     }
 }
